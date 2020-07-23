323962858dbe069fb25f688acc8ace0839eb1c03
Tymm Schmitke <ifellinaholeonce@gmail.com>
2020-07-23 08:17:01 -0400

    calculate one step out

--- app/snake.rb
+++ app/snake.rb
@@ -11,13 +11,29 @@ class Snake
 
     puts "Head: #{head}"
 
-    move = possible_moves.sample
-    puts "MOVE: #{move}"
+    possible_moves = possible_moves_from_square(head[:x], head[:y])
 
+    # will a random direction have a move after? dont trap yourself
+    move = calculate_next_step(possible_moves)
+
+    puts "MOVE: #{move}"
     { "move": move }
   end
 
   private
+
+  def calculate_next_step(possible_moves)
+    dir = possible_moves.sample
+    new_head_coords = mutate_coords_by_dir(head[:x], head[:y], dir)
+    if possible_moves_from_square(new_head_coords[:x], new_head_coords[:y])
+      dir
+    else
+      possible_moves.delete(dir)
+      return dir if possible_moves.empty?
+
+      calculate_next_step(possible_moves)
+    end
+  end
 
   def direction_hash(x, y)
     {
@@ -28,26 +44,35 @@ class Snake
     }
   end
 
-  def possible_moves
+  def possible_moves_from_square(x, y)
     possible_moves = []
     direction_hash(head[:x], head[:y]).each do |k ,v|
       # don't run into tail
       next if snake[:body].include?(v)
       # don't leave board
-      next if outside_board?(v)
+      next if outside_board?(v[:x], v[:y])
 
       possible_moves << k.to_s
     end
     possible_moves
   end
 
-  def moves_from_square(coords)
-    # check each direction for empty space
+  def mutate_coords_by_dir(x, y, dir)
+    coords = {x: x, y: y}
+    case dir
+    when "up"
+      coords[:y] -= 1
+    when "down"
+      coords[:y] += 1
+    when "left"
+      coords[:x] -= 1
+    when "right"
+      coords[:x] += 1
+    end
+    coords
   end
 
-  def outside_board?(coords)
-    x = coords[:x]
-    y = coords[:y]
+  def outside_board?(x, y)
     return true if x.negative? || x >= board[:width]
 
     return true if y.negative? || y >= board[:height]
