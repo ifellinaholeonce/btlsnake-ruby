9863d01709f14a2f68add67eefa9a36d270e4c06
Tymm Schmitke <ifellinaholeonce@gmail.com>
2020-07-23 07:36:05 -0400

    refactor direction_hash

--- app/snake.rb
+++ app/snake.rb
@@ -1,51 +1,57 @@
 class Snake
   attr_accessor :snake, :board, :head
-  def initialize()
-  end
+
+  def initialize(); end
 
   def move(request)
+    puts(request)
     self.snake = request[:you]
     self.board = request[:board]
     self.head = snake[:head]
 
     puts "Head: #{head}"
 
-    legitimate_moves = []
-
-    possible_moves.each do |k ,v|
-      # don't run into tail
-      next if snake[:body].include?(v)
-      puts "#{snake[:body]} does not include #{v}"
-      # don't leave board
-      next if outside_board?(v)
-      puts "#{v} is not outside the board"
-      legitimate_moves << k.to_s
-    end
-
-    move = legitimate_moves.sample
-    puts "move_list:"
-    puts legitimate_moves
-    puts "MOVE: " + move
+    move = possible_moves.sample
+    puts "MOVE: #{move}"
 
     { "move": move }
   end
 
   private
 
-  def possible_moves
+  def direction_hash(x, y)
     {
-      up:     { x: head[:x], y: head[:y] - 1 },
-      down:   { x: head[:x], y: head[:y] + 1 },
-      left:   { x: head[:x] - 1, y: head[:y] },
-      right:  { x: head[:x] + 1, y: head[:y] }
+      up: { x: x, y: y - 1 },
+      down: { x: x, y: y + 1 },
+      left: { x: x - 1, y: y },
+      right: { x: x + 1, y: y }
     }
+  end
+
+  def possible_moves
+    possible_moves = []
+    direction_hash(head[:x], head[:y]).each do |k ,v|
+      # don't run into tail
+      next if snake[:body].include?(v)
+      # don't leave board
+      next if outside_board?(v)
+
+      possible_moves << k.to_s
+    end
+    possible_moves
+  end
+
+  def moves_from_square(coords)
+    # check each direction for empty space
   end
 
   def outside_board?(coords)
     x = coords[:x]
     y = coords[:y]
-    return true if x < 0 || x >= board[:width]
-    return true if y < 0 || y >= board[:height]
-    return false
+    return true if x.negative? || x >= board[:width]
+
+    return true if y.negative? || y >= board[:height]
+
+    false
   end
 end
